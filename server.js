690948a6478797e04d979e1a16a4136de692cfea
tsip <glavermelaver@gmail.com>
2021-08-01 16:05:28 +0300

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData=[
     {name: 'Tsip'},
@@ -13,6 +16,7 @@ app.get('/users', function(req, res) {
     })
 })
 
+//colons are used as variable that be viewed in the params
 app.get('/users/:id', function(req, res) {
     console.log(req.params.id)
     res.json({
@@ -20,6 +24,28 @@ app.get('/users/:id', function(req, res) {
         message: 'got one user',
         user: req.params.id
     })
+})
+
+app.post('/login', function(req, res){
+    //Typically passwords are encrypted using something like bcrypt before sending to database
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyThaKid";
+    const mockPassword = "superSecret";
+    
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+        } else {
+            res.json({
+                success: false,
+                message: 'password and username do not match'
+            })
+        }
 })
 
 app.listen(8000, function() {
